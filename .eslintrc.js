42c7c5339298aae7022f8ee202c99701c7851362
Eduard-Pop <eduardedipop@gmail.com>
2024-03-05 01:48:15 +0100

    Bug Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
-  rules:{
+  rules: {
     'prettier/prettier': ['warn', { endOfLine: 'auto' }],
     'no-console': 'off',
-  }
+  },
 }
